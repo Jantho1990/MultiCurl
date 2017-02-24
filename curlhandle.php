439de1f3e0ee00d6c1b936901591bc8c15c215d5
Joshua Anthony <jantho1990@gmail.com>
2017-02-24 00:01:01 -0600

    started fleshing out curlhandle, including curlopt loop

--- curlhandle.php
+++ curlhandle.php
@@ -20,6 +20,14 @@ class CurlHandle {
   // The curl handle itself.
   private $ch = null;
 
+  /**
+   *  Return the raw curl handle.
+   *  @return curl object
+   */
+  public function getHandle(){
+    return $this->ch;
+  }
+
   public function __construct($url, $curl_opts, $id){
     // Validate arguments.
     $args = func_get_args();
@@ -35,11 +43,24 @@ class CurlHandle {
     curl_setopt($ch, CURLOPT_URL, $url);
     $this->id = $id;
     curl_setopt($ch, CURLOPT_PRIVATE, $id);
+    foreach($curl_opts as $co=>$curl_opt){
+      switch($co){
+        case 'CURLOPT_PRIVATE':
+          // Not setting this because that's what id is for.
+          break;
+        case 'CURLOPT_URL':
+          // Use $url to set this.
+          break;
+        default:
+          curl_setopt($ch, $co, $curl_opt);
+      }
+    }
+    return $this;
 
   }
 
   public function __destruct(){
-
+    curl_close($this->ch);
   }
 
 }
